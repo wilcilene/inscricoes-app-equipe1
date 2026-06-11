bee5af83488633ba06ed36075cfc468b6cfd337d
felipeee98 <felipeee987172@gmail.com>
2026-06-11 08:36:58 -0300

    Ajusta login e rotas principais

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
-<?php
+<?php<?php
 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('arquivo');
 });
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/candidaturas', function () {
+    return view('candidaturas');
+})->name('candidaturas');
 
 require __DIR__ . '/candidato.php';
