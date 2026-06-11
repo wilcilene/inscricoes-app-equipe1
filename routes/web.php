7bd7df4e8ce06a3ac4b6f7e41ed6f1e2bb448586
Renan Ramlov <76700320+renanramlov@users.noreply.github.com>
2026-06-11 15:42:02 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< refs/remotes/origin/Kalleb---Laravel-com-tela-de-login-e-inscrição-ok
-#candidato
-Route::get('/', function () {
-    return view('arquivo');
-=======
 
+use App\Http\Controllers\CandidatoController;
+
+Route::post('/candidato/endereco', [CandidatoController::class, 'salvarEndereco'])
+    ->name('candidato.endereco');
+
+// Página inicial
 Route::get('/', function () {
     return view('login');
->>>>>>> local
 });
-#candidato
+
+// Perfil
 Route::get('/perfil', function () {
     return view('perfil');
 });
-<<<<<<< refs/remotes/origin/Kalleb---Laravel-com-tela-de-login-e-inscrição-ok
-=======
 
+// Arquivo
+Route::get('/arquivo', function () {
+    return view('arquivo');
+});
+
+// Confirmação
 Route::get('/confirmacao', function () {
     return view('confirmacao');
 });
 
+// Inscrição
 Route::get('/inscricao', function () {
     return view('inscricao');
 });
->>>>>>> local
